a27e618304fdd36f232dddf0facd5a2b002c3263
Brian Manning <bri@brimanning.com>
2020-02-01 10:54:59 -0500

    Move to snake case and update formatting

--- monitor.py
+++ monitor.py
@@ -1,7 +1,7 @@
 import subprocess
 import time
 
-def getConnectedDevices():
+def get_connected_devices():
     p = subprocess.Popen("arp-scan -l", stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     p_status = p.wait()
@@ -11,22 +11,22 @@ def getConnectedDevices():
             if (line.startswith("192")):
                 addresses.append(line[line.index(":")-2:line.rindex(":")+3])
     else:
-        print "Error: " + err
+        print("Error: " + err)
     return addresses
 
-oldAddresses = getConnectedDevices()
+old_addresses = get_connected_devices()
 
 while True:
-    newAddresses = getConnectedDevices()
+    new_addresses = get_connected_devices()
         
-    for address in newAddresses:
-        if address not in oldAddresses:
-            print "Device joined: " + address
+    for address in new_addresses:
+        if address not in old_addresses:
+            print("Device joined: " + address)
 
-    for address in oldAddresses:
-        if address not in newAddresses:
-            print "Device left  : " + address
+    for address in old_addresses:
+        if address not in new_addresses:
+            print("Device left  : " + address)
 
-    oldAddresses = newAddresses
+    old_addresses = new_addresses
 
     time.sleep(5)
